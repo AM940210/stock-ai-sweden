172c4c28b8c8f63bf03817307a0cacde53ac1d10
Ahmad Mohammadi <ahmmoh0210@gmail.com>
2026-07-24 08:21:24 +0200

    improved api route with prevent duplicate stocks

--- app/api/stocks/route.ts
+++ app/api/stocks/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/src/lib/prisma";
+import { error } from "console";
 
 export async function GET() {
     const stocks = await prisma.stock.findMany({
@@ -12,19 +13,46 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-    const body = await req.json();
+    try{
+        const body = await req.json();
 
-    const stock = await prisma.stock.create({
-        data: {
-            symbol: body.symbol,
-            companyName: body.companyName,
-            market: body.market,
-            sector: body.sector,
-            industry: body.industry,
-            currentPrice: body.currentPrice,
-            currency: body.currency,
-        },
-    });
+        // Check if the stock already exists
+        const existingStock = await prisma.stock.findUnique({
+            where: {
+                symbol: body.symbol,
+            },
+        });
 
-    return NextResponse.json(stock, { status: 201 });
+        if (existingStock) {
+            return NextResponse.json(
+                {
+                    error: `Stock ${body.symbol} already exists.`,
+                },
+                { status: 409 }
+            );
+        }
+
+        const stock = await prisma.stock.create({
+            data: {
+                symbol: body.symbol,
+                companyName: body.companyName,
+                market: body.market,
+                sector: body.sector,
+                industry: body.industry,
+                currentPrice: body.currentPrice,
+                currency: body.currency,
+            },
+        });
+
+        return NextResponse.json(stock, { status: 201 });
+    } catch (error) {
+        console.error(error);
+
+        return NextResponse.json(
+            {
+                error: "Something went wrong.",
+            },
+            { status: 500 }
+        );
+    }
 }
